4255ba22ac2fd0d73ca59618a62a6908c7d73efd
David <dhblodgett@gmail.com>
2016-04-26 07:39:46 -0700

    used jQuery and Github API

---
+++ my_github_profile.js
@@ -0,0 +1,76 @@
+/**
+ * Created by davidblodgett on 4/25/16.
+ */
+
+$(document).ready(function(){
+    function getUserProfile(username) {
+        $.ajax({
+            url: 'https://api.github.com/users/' + username,
+            method: 'get',
+            success: successUser,
+            error: errorUser,
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', 'Token ' + '6c802387702476b472435014159a0257e967c1d9');
+            }
+        });
+    }
+
+    function errorUser(err) {
+        console.log(err.responseText);
+    }
+
+    function successUser(resp) {
+        var userProfile = `
+                <img src='${resp.avatar_url}' height="150" width="150"/>
+                <h2>${resp.name} </h2 >
+                <h2>${resp.login} </h2>
+                `;
+
+        var userDetails = `
+                <l>
+                <li><i class="fa fa-map-marker"></i>$(resp.location)</li>
+                <li><i class="fa fa-envelope-o"></i> $(resp.email)</li>
+                <li><i class="fa fa-link"></i>$(resp.blog)</li>
+                <li><i class="fa fa-clock-o"></i>$(resp.created_at)</li>
+                </l>
+                `;
+
+        $('#user_profile').html(userProfile);
+        $('#user_details').html(userDetails);
+
+    }
+
+    function getRepositories(username) {
+        $.ajax({
+            url: 'https://api.github.com/users/' + username + '/repos',
+            method: 'get',
+            success: successRepo,
+            error: errorRepo,
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', 'Token ' + '6c802387702476b472435014159a0257e967c1d9');
+            }
+                // headers: {
+                    // 'Authorization': 'token  ${token}
+        });
+    }
+
+    function successRepo(resp) {
+        // console.log('resp',resp);
+        resp.forEach(function (item) {
+            console.log('item',item);
+                $("#repositories").append(
+                    `<li><a href=${item.html_url}>${item.name}</a>
+                    <p>updated at ${item.updated_at}</p>
+                    </li>`
+                )
+            });
+        }
+
+    function errorRepo(err) {
+        console.log(err.responseText);
+        }
+
+    getUserProfile('D4VEB');
+    getRepositories('D4VEB');
+
+});
